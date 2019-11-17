4189848a6ead234a0956ee32a529d95b34a6f244
hartred <artiomse@gmail.com>
2019-11-17 18:33:49 +0300

    Added first test

---
+++ test.py
@@ -0,0 +1,29 @@
+import json
+import os
+
+import pytest
+
+from app import Application
+
+
+@pytest.fixture
+def test_data_file() -> None:
+    disciplines = [
+        'string1',
+        'string2',
+        'string3',
+    ]
+    with open('disciplines.json', 'w') as file:
+        json.dump(disciplines, file)
+    yield 'disciplines.json'
+    os.remove('disciplines.json')
+
+def test_discipline_data_returns_dict_from_json(test_data_file):
+    app = Application('.')
+    expected = [
+        'string1',
+        'string2',
+        'string3',
+    ]
+    result = app.parse_disciplines()
+    assert expected == result
