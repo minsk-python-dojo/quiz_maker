4230c62fdb5c89f671201f8c136731963d66356c
hartred <artiomse@gmail.com>
2019-11-17 18:32:56 +0300

    Added application class with methods

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
+import os
+import json
+
+
 class Application:
     def __init__(self, data_path: str):
         self.data_path = data_path
 
     def run(self):
         print(f'config path: {self.data_path}')
+
+    def parse_disciplines(self):
+        disciplines_data_path = os.path.join(self.data_path, "disciplines.json")
+        f = open(disciplines_data_path, 'r')
+        disciplines_data = json.load(f)
+        f.close()
+        return disciplines_data
+
+
+        
+    
+            
+
+
