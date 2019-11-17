9bf49d530a6d670ea0a1de6422ee32cf53ad6bfa
hartred <artiomse@gmail.com>
2019-11-17 17:14:53 +0300

    added checking enter parameters to the main.py

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+#!/usr/bin/env python3
+import os
+import sys
+
 import quiz
 
+from app import Application
+
+
+def main(data_path: str) -> None:
+    quiz_app = Application(data_path)
+    quiz_app.run()
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) < 2:
+        print('Error! At least 1 argument expected. Aborting.')
+        sys.exit(1)      
+    data_path = sys.argv[1]
+    if not os.path.exists(data_path):
+        print(f'Error! {data_path} is not a valid path. Aborting.')
+        sys.exit(1)
+    main(data_path)
+    
